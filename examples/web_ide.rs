49719cd85b883894070905846c6cf93a738676d5
Juuso Haavisto <juuso@mail.com>
2022-02-23 12:06:34 +0000

    add smt via podman solve, use async on frontend

--- examples/web_ide.rs
+++ examples/web_ide.rs
@@ -5,17 +5,36 @@ use apl_pest::parse;
 use rivi_loader::DebugOption;
 
 #[fn_handler]
-async fn caps(req: &mut Request, res: &mut Response) {
+async fn caps(_req: &mut Request, res: &mut Response) {
     let vk = rivi_loader::new(DebugOption::None).unwrap();
     res.render_plain_text(&format!("{}", vk));
 }
 
 #[fn_handler]
-async fn savilerow(req: &mut Request, res: &mut Response) {
+async fn savilerow(_req: &mut Request, res: &mut Response) {
     let mut cursor = std::io::Cursor::new(&include_bytes!("./vulkan.eprime")[..]);
     let mut model = String::new();
     cursor.read_to_string(&mut model).unwrap();
     res.render_plain_text(&model);
+}
+
+#[fn_handler]
+async fn solve(req: &mut Request, res: &mut Response) {
+    let form = req.form_data().await.unwrap();
+    let param = form.fields.get("param").unwrap().trim();
+
+    let idris = Command::new("podman")
+        .arg("run")
+        .arg("savilerow")
+        .arg(format!("{}/examples/vulkan.eprime", env!("CARGO_MANIFEST_DIR")))
+        .arg("-params")
+        .arg(param)
+        .arg("-run-solver")
+        .arg("-solutions-to-stdout")
+        .output()
+        .expect("failed to execute savilerow");
+
+    res.render_plain_text(&from_utf8(&idris.stdout).unwrap().trim());
 }
 
 #[fn_handler]
@@ -50,6 +69,7 @@ async fn main() {
     )
     .push(Router::with_path("smt")
         .get(savilerow)
+        .post(solve)
     );
     Server::new(TcpListener::bind("127.0.0.1:7878")).serve(router).await;
 }
